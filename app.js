ea42b7d626ec00809a7810f59a37a54271d21066
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2022-11-16 12:06:27 -0300

    listando registros de localStorage

--- app.js
+++ app.js
@@ -39,6 +39,29 @@ class Bd {
 
         localStorage.setItem('id', id)
     }
+
+    recuperarTodosRegistros() {
+        let id = localStorage.getItem('id')
+
+        //array de despesas
+        let despesas = Array()
+
+        //recuperar todas as despesas cadastradas em localStorage
+        for(let i = 1; i <= id; i++) {
+
+            //recuperar a despesa
+            let despesa = JSON.parse(localStorage.getItem(i))
+
+            //existe a possibilidade de haver indices que foram removidos
+            if(despesa === null) {
+                continue
+            }
+
+            despesas.push(despesa)
+        }
+
+        return despesas
+    }
 }
 
 let bd = new Bd()
@@ -55,7 +78,7 @@ function cadastrarDespesa() {
     let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
     
     if(despesa.validarDados()) {
-        // bd.gravar(despesa)
+        bd.gravar(despesa)
         
         $('#modalTitulo').html('Registro inserido com sucesso')
         $('#modalTituloDiv').addClass('modal-header text-success')
@@ -64,13 +87,61 @@ function cadastrarDespesa() {
         $('#modalBtn').addClass('btn btn-success')
 
         $('#modalRegistrarDespesa').modal('show')
+
     } else {
         $('#modalTitulo').html('Erro na insclusão do registro')
         $('#modalTituloDiv').addClass('modal-header text-danger')
         $('#modalConteudo').html('<p>Erro na gravação, verifique os campos</p>')
         $('#modalBtn').html('Voltar e corrigir')
         $('#modalBtn').addClass('btn btn-danger')
-        
+
         $('#modalRegistrarDespesa').modal('show')
+
     }
+}
+
+function carregaListaDespesas() {
+
+    let despesas = Array()
+    despesas = bd.recuperarTodosRegistros()
+
+    //selecionando o elemento tbody da tabela
+    let listaDespesas = document.getElementById('listaDespesas')
+
+
+//     <tr>
+//     <td>15/04/2022</td>
+//     <td>Alimentaçao</td>
+//     <td>compras do mes</td>
+//     <td>17.09</td>
+//   </tr>
+
+    //percorrer o array despesas, listando cada despesa de forma dinamica
+    despesas.forEach(function(d) {
+        
+        console.log(d)
+        //criando tr
+        let linha = listaDespesas.insertRow()
+
+        //criando td
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+        
+        //ajustar o tipo
+        switch(parseInt(d.tipo)) {
+            case 1: d.tipo = 'Alimentação'
+                break
+            case 2: d.tipo = 'Educação'
+                break
+            case 3: d.tipo = 'Lazer'
+                break
+            case 4: d.tipo = 'Saúde'
+                break
+            case 5: d.tipo = 'Transporte'
+                break
+        }
+
+        linha.insertCell(1).innerHTML = d.tipo
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = d.valor
+    })
 }
