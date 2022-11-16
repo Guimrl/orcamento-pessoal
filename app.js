2448c7bdf432328ec30059e5b31cc1637594c8cd
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2022-11-16 12:23:35 -0300

    criando metodo de pesquisa de despesas

--- app.js
+++ app.js
@@ -62,6 +62,10 @@ class Bd {
 
         return despesas
     }
+
+    pesquisar(despesa) {
+        console.log(despesa)
+    } 
 }
 
 let bd = new Bd()
@@ -78,7 +82,7 @@ function cadastrarDespesa() {
     let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
     
     if(despesa.validarDados()) {
-        bd.gravar(despesa)
+        // bd.gravar(despesa)
         
         $('#modalTitulo').html('Registro inserido com sucesso')
         $('#modalTituloDiv').addClass('modal-header text-success')
@@ -153,4 +157,17 @@ function carregaListaDespesas() {
         linha.insertCell(2).innerHTML = d.descricao
         linha.insertCell(3).innerHTML = d.valor
     })
+}
+
+function pesquisarDespesa() {
+    let ano = document.getElementById('ano').value
+    let mes = document.getElementById('mes').value
+    let dia = document.getElementById('dia').value
+    let tipo = document.getElementById('tipo').value
+    let descricao = document.getElementById('descricao').value
+    let valor = document.getElementById('valor').value
+
+    let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+    
+    bd.pesquisar(despesa)
 }
